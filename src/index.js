809fd8b408d5fb2644d3b08f24ccab2d6c90328c
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-02-05 14:15:16 +0200

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const OPEN_BRACKETS = ["(", "{", "|"];
+  const BRACKETS_PAIR = {
+    [")"]: "(",
+    ["}"]: "{",
+    ["|"]: "|",
+  };
+
+  // function isBracketsOk(str) {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      if (bracketsConfig.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
 }
