35c907b93d5b6c1989f7aa96e61df5b8351ad83c
anna-blumenkrantz <91938764+anna-blumenkrantz@users.noreply.github.com>
2023-02-05 16:54:13 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
 
-   let stack = [];
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < bracketsConfig.length; j++) {
-      // Check if the current character is an opening bracket
-      if (str[i] === bracketsConfig[j][0]) {
-        stack.push(str[i]);
-        break;
-      }
-      // Check if the current character is a closing bracket
-      if (str[i] === bracketsConfig[j][1]) {
+  // console.log('Brackets issue:');
+  //store open brackets 
+  const OPEN_BRACKETS = bracketsConfig.map((bracketPair) => bracketPair[0]);
+  // console.log('OPEN_BRACKETS' +OPEN_BRACKETS);
+  //make a map of pairs  
+  const BRACKETS_PAIR = {};
+
+    for (const bracketPair of bracketsConfig) {
+      BRACKETS_PAIR[bracketPair[0]] = bracketPair[1];
+    } 
+  // const BRACKETS_PAIR = new Map(bracketsConfig.map(([open, close]) => [open, close]));
+  // console.log('BRACKETS_PAIR' +JSON.stringify(BRACKETS_PAIR));
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      // console.log('cur** '+currentSymbol);
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+        // console.log('stack push ***'+stack);
+      } else {
         if (stack.length === 0) {
           return false;
         }
-        // Check if the last opened bracket matches the current closing bracket
-        if (stack[stack.length - 1] !== bracketsConfig[j][0]) {
+
+        let topElement = stack[stack.length - 1];
+        // console.log('topElement** '+topElement);
+
+        // if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        if (BRACKETS_PAIR[topElement] === currentSymbol) {
+          // console.log('====** '+BRACKETS_PAIR[currentSymbol] === topElement);
+          stack.pop();
+          // console.log('POP'+stack);
+        } else {
           return false;
         }
-        stack.pop();
-        break;
       }
     }
-  }
-  return stack.length === 0;
+ return stack.length === 0;
+
 }
+// const config1 = [['(', ')']];
+// const config2 = [['(', ')'], ['[', ']']];
+// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+// const config4 = [['|', '|']];
+// const config5 = [['(', ')'], ['|', '|']];
+// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+// check('||', config4);
